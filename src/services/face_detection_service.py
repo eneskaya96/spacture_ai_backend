888d52dbd514e9c92faa44000fa0851081911ad5
eneskaya96 <enes.kaya95@hotmail.com>
2023-12-17 15:28:31 +0300

    no need uow in here

--- src/services/face_detection_service.py
+++ src/services/face_detection_service.py
@@ -57,6 +57,4 @@ class FaceDetectionService(BaseService):
         Get all face detections for a company
         :param company_id
         """
-        with self.uow:
-            face_detections = self.uow.face_detection.get_face_detection_by_company_id(company_id)
-        return face_detections
+        return self.uow.face_detection.get_face_detection_by_company_id(company_id)
