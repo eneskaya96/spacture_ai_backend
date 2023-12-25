7aa3b6701cdac41c5ff4ad49313cd9cc0dfb0739
eneskaya96 <enes.kaya95@hotmail.com>
2023-12-25 23:40:39 +0300

    watchlist_face_detection pagination

--- src/infrastructure/persistence/repositories/watchlist/watchlist_face_detection_repository.py
+++ src/infrastructure/persistence/repositories/watchlist/watchlist_face_detection_repository.py
@@ -1,5 +1,7 @@
 from typing import Optional, Dict, List, Type
 
+from sqlalchemy import desc
+
 from src.domain.watchlist.entities.watchlistFaceDetection import WatchlistFaceDetection as DomainWatchlistFaceDetection
 from src.domain.watchlist.repositories.watchlist_face_detection_repository import WatchlistFaceDetectionRepository as WatchlistFaceDetectionDomainRepository
 from src.infrastructure.entities.watchlist.watchlist_face_detection import WatchlistFaceDetection
@@ -11,5 +13,10 @@ class WatchlistFaceDetectionRepository(BaseGenericRepository[DomainWatchlistFace
     def __init__(self) -> None:
         super().__init__(WatchlistFaceDetection, DomainWatchlistFaceDetection)
 
-    def get_watchlist_face_detections(self, watchlist_ids: List[str]) -> list[Type[WatchlistFaceDetection]]:
-        return self.session.query(WatchlistFaceDetection).filter(WatchlistFaceDetection.watchlist_id.in_(watchlist_ids)).all()
+    def get_watchlist_face_detections(self, watchlist_ids: List[str], limit, offset) -> list[Type[WatchlistFaceDetection]]:
+        return self.session.query(WatchlistFaceDetection)\
+            .filter(WatchlistFaceDetection.watchlist_id.in_(watchlist_ids))\
+            .order_by(desc(WatchlistFaceDetection.created_date))\
+            .limit(limit) \
+            .offset(offset) \
+            .all()
